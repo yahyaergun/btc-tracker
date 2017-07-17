fb4578c4c29a0812211e19870a31e7aec5fcf16d
Yahya Ergün <yahya.ergun@sahibinden.com>
2017-07-17 13:37:09 +0300

    currency @ykb

--- src/main/java/com/ergun/btc/Constants.java
+++ src/main/java/com/ergun/btc/Constants.java
@@ -9,11 +9,11 @@ public interface Constants {
     String BTCTURK_URL = "https://www.btcturk.com/";
     String KOINIM_URL = "https://koinim.com/ticker/";
     String PARIBU_URL = "https://www.paribu.com/endpoint/state";
-    String DOVIZ_URL = "http://www.doviz.com/api/v1/currencies/USD/latest";
+    String DOVIZ_URL = "https://kur.doviz.com/api/v1/currencies/USD/latest/yapikredi";
 
     Price YKB_SWIFT_COST = new Price(50.00D, Currency.TRY);
     Double BITSTAMP_BTC_USD_FEE_PERC = 0.0025D; // %0.25
-    Double BITSTAMP_DEPOSIT_FEE_PERC = 0.0005D; // %0.5
+    Double BITSTAMP_DEPOSIT_FEE_PERC = 0.005D; // %0.5s
     Double BTCTURK_TAKER_FEE_PERC = 0.004D; // %0.4
     Double BTCTURK_MAKER_FEE_PERC = 0.001D; // %0.1
 }
